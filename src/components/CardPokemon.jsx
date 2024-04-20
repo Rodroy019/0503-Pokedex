d8b8abcd8565e868f1112a8bab4feebe1abfaa23
Rodrigo Rivas <rodroy019@hotmail.com>
2024-04-20 19:17:53 -0400

    finish: se termina proyecto de API Pokemon

--- src/components/CardPokemon.jsx
+++ src/components/CardPokemon.jsx
@@ -1,54 +1,71 @@
-import { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Card from 'react-bootstrap/Card'
 import ListGroup from 'react-bootstrap/ListGroup'
 import { useParams } from 'react-router-dom'
 
 const CardPokemon = () => {
   const { name } = useParams()
-  const [pokemon, setPokemon] = useState('')
-
-  const getPokemon = async () => {
-    try {
-      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
-      const data = await response.json()
-      console.log(data)
-      const idPokemon = data.id
-      const imgPokemon = data.sprites.other['official-artwork'].front_default
-      const abilitiesPokemon = data.abilities.map((item) => item.ability.name)
-      const typePokemon = data.types.map((tipo) => tipo.type.name)
-
-      const pokemonInfo = {
-        id: idPokemon,
-        img: imgPokemon,
-        abilities: abilitiesPokemon,
-        types: typePokemon
-      }
-      setPokemon(pokemonInfo)
-    } catch (error) {
-      console.log(error)
-    }
-  }
+  const [pokemon, setPokemon] = useState(null)
 
   useEffect(() => {
+    const getPokemon = async () => {
+      try {
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
+        const data = await response.json()
+
+        const idPokemon = data.id
+        const imgPokemon = data.sprites.other['official-artwork'].front_default
+        const abilitiesPokemon = data.abilities.map((item) => item.ability.name)
+        const typePokemon = data.types.map((tipo) => tipo.type.name)
+
+        const pokemonInfo = {
+          id: idPokemon,
+          img: imgPokemon,
+          abilities: abilitiesPokemon,
+          types: typePokemon
+        }
+
+        setPokemon(pokemonInfo)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+
     getPokemon()
   }, [name])
 
   return (
     <div className='image-container'>
-      <Card style={{ width: '25rem' }}>
-        <Card.Img variant='top' src={pokemon.img} />
-        <Card.Body>
-          <Card.Title>Nombre: {name.toUpperCase()} </Card.Title>
-          <Card.Text>
-            Posicion en Pokedex <span>{pokemon.id}</span>.
-          </Card.Text>
-        </Card.Body>
-        <ListGroup className='list-group-flush'>
-          <ListGroup.Item>Tipo: {pokemon.types}</ListGroup.Item>
-          <ListGroup.Item>Habilidades: {pokemon.abilities}</ListGroup.Item>
-        </ListGroup>
-      </Card>
-
+      {pokemon && (
+        <Card style={{ width: '25rem' }}>
+          <Card.Img variant='top' src={pokemon.img} alt={name} />
+          <Card.Body>
+            <Card.Title>Nombre: {name.toUpperCase()}</Card.Title>
+            <Card.Text>
+              Posición en Pokédex: <span>{pokemon.id}</span>
+            </Card.Text>
+          </Card.Body>
+          <ListGroup className='list-group-flush'>
+            <ListGroup.Item>
+              Tipo:
+              <ul>
+                {pokemon.types.map((type) => (
+                  <li key={type}>{type}</li>
+                ))}
+              </ul>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              Habilidades:
+              <ul>
+                {pokemon.abilities.map((ability) => (
+                  <li key={ability}>{ability}</li>
+                ))}
+              </ul>
+            </ListGroup.Item>
+          </ListGroup>
+        </Card>
+      )}
+      {!pokemon && <div>Cargando información del Pokémon...</div>}
     </div>
   )
 }
--- src/views/Pokemon.jsx
+++ src/views/Pokemon.jsx
@@ -1,11 +1,8 @@
-import { useParams } from 'react-router-dom'
 import CardPokemon from '../components/CardPokemon'
 
 const Pokemon = () => {
-  const { name } = useParams()
   return (
     <div className='image-container'>
-      <h4>Tu pokemon es: {name.toUpperCase()}</h4>
       <CardPokemon />
     </div>
   )
